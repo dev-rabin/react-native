c38725495f6f38b7e233d21c73dfd5cf25734e3f
dev-rabin <robinmandhotia@gmail.com>
2024-06-07 19:48:53 +0530

    section list completed

--- flatlist/app/(tabs)/index.js
+++ flatlist/app/(tabs)/index.js
@@ -1,22 +1,21 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, SectionList, StyleSheet, Text, View } from "react-native";
 import PokemonList from "../../data.json"
 import { SafeAreaView } from "react-native-safe-area-context";
+import groupedPokemonList from "../../groupedData.json";
 
 export default function HomeScreen() {
 
   return (
-    <SafeAreaView style={style.safeAreaView}>
-    <Text style={style.heading}>
-      Pokemons
-    </Text>
-      <FlatList data={PokemonList}
+    <SafeAreaView style={style.safe}>
+      <View>
+        {/* <FlatList data={PokemonList}
       keyExtractor={(item => item.id.toString())}
         renderItem={({ item }) => {
           console.log(item);
           return (
           <View style={style.container}>
               <View style={style.card} key={item.id}>
-              <Text style={style.name}>
+              <Text style={style.cardText}>
                 {item.name}
               </Text>
               <Text style={style.type}>
@@ -26,41 +25,89 @@ export default function HomeScreen() {
       </View>
           )
         }}
+        ListEmptyComponent={()=> <Text style={style.empty}>
+        No items found
+      </Text>}
+      ListHeaderComponent={() => <Text style={style.headerText}>Pokemons List</Text>}
+      ListFooterComponent={() => <Text style={style.footerText}>End of Pokemon List</Text>}
+      /> */}
+
+      <SectionList
+        sections={groupedPokemonList}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => {
+          return (
+            <View style={style.card}>
+              <Text style={style.cardText}>{item}</Text>
+            </View>
+          );
+        }}
+        renderSectionHeader={({ section }) => {
+          return (
+            <View style={style.sectionHeader}>
+              <Text style={style.sectionText}>{section.type}</Text>
+            </View>
+          );
+        }}
+        ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+        SectionSeparatorComponent={() => <View style={{ height: 10 }} />}
       />
+      </View>
     </SafeAreaView>
   )
 }
 
 const style = StyleSheet.create({
-  card : {
-    borderWidth : 1,
-    padding : 18,
-    borderColor : "black",
-    margin : 2,
-    borderRadius : 8,
-    alignItems : "center",
-    backgroundColor : "white",
-    elevation : 5,
+  safe: {
+    paddingHorizontal: 16
   },
-  name : {
-    fontSize : 26,
+  card: {
+    borderWidth: 1,
+    padding: 18,
+    borderColor: "black",
+    // margin : 2,
+    borderRadius: 8,
+    alignItems: "center",
+    backgroundColor: "white",
+    elevation: 5,
+  },
+  cardText: {
+    fontSize: 26,
+    // fontWeight: "bold",
+  },
+  type: {
+    color: "grey",
+    fontSize: 16,
+  },
+  heading: {
+    fontSize: 34,
     fontWeight: "bold",
+    alignSelf: "center",
+    textDecorationLine: "underline",
+    color: "blue",
   },
-  type : {
-    color : "grey",
-    fontSize :16,
+  container: {
+    margin: 16
   },
-  safeAreaView : {
-    backgroundColor : "#f5f5f5",
+  empty: {
+    flex: 1,
+    fontSize: 40,
+    alignSelf: "center"
   },
-  heading : {
-    fontSize : 34,
-    fontWeight : "bold",
-    alignSelf : "center",
-    textDecorationLine : "underline",
-    color : "blue",
+  headerText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    alignSelf: "center",
+    color: "blue"
   },
-  container : {
-    margin : 6
+  footerText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    alignSelf: "center",
+  },
+  sectionText: {
+    color: "black",
+    fontSize: 20,
+    marginVertical: 12
   }
 })
