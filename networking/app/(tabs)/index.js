f095a4901e091a5d67fe885651c95c4841231272
dev-rabin <robinmandhotia@gmail.com>
2024-06-09 17:28:26 +0530

    error handling in networking

--- networking/app/(tabs)/index.js
+++ networking/app/(tabs)/index.js
@@ -3,21 +3,28 @@ import { ActivityIndicator, Button, FlatList, SafeAreaView, StatusBar, StyleShee
 
 export default function App() {
   const [posts, setPosts] = useState([]);
-  const [isLoading , setIsLoading] = useState(true);
-  const [refreshing , setRefreshing] = useState(false);
-  const [postForm , setPostForm] = useState({
-    title : "",
-    body : ""
+  const [isLoading, setIsLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+  const [postForm, setPostForm] = useState({
+    title: "",
+    body: ""
   })
-  const [isPosting , setIsPosting] = useState(false);
+  const [isPosting, setIsPosting] = useState(false);
+  const [error, setError] = useState("");
 
   const fetchPosts = async (limit = 10) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
-    console.log("fetchpost response : ", response);
-
-    const fetchData = await response.json();
-    setPosts(fetchData);
-    setIsLoading(false);
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
+      console.log("fetchpost response : ", response);
+      const fetchData = await response.json();
+      setPosts(fetchData);
+      setIsLoading(false);
+      setError("")
+    } catch (error) {
+      console.error("Fetch post error : ", error);
+      setError("Error! Post Not Fetched.");
+      setIsLoading(false);
+    }
   }
 
   const handleRefresh = () => {
@@ -33,131 +40,155 @@ export default function App() {
   if (isLoading) {
     return (
       <SafeAreaView style={style.loader}>
-        <ActivityIndicator animating size="large" color="black"/>
+        <ActivityIndicator animating size="large" color="black" />
         <Text>Loading...</Text>
       </SafeAreaView>
     )
   }
 
-  const handleInputChange = (name, value) =>{
-    setPostForm({...postForm , [name] : value});
+  const handleInputChange = (name, value) => {
+    setPostForm({ ...postForm, [name]: value });
   }
 
   const addPost = async () => {
-    setIsPosting(true);
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      method : "POST",
-      headers : {
-        "Content-Type" : "application/json"
-      },
-      body : JSON.stringify(postForm)
-    });
-    const newPost = await response.json();
-    console.log("New post : ", newPost);
-    setPosts([newPost, ...posts]);
-    setPostForm("");
-    setIsPosting(false);
+    try {
+      setIsPosting(true);
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postForm)
+      });
+      const newPost = await response.json();
+      console.log("New post : ", newPost);
+      setPosts([newPost, ...posts]);
+      setPostForm("");
+      setIsPosting(false);
+    } catch {
+      console.error("Error! To adding post", error);
+      setError("Error! To adding post");
+    }
   }
 
 
   return (
     <SafeAreaView style={style.container}>
-
-    <View style={style.textInput}>
-      <TextInput style={style.input} 
-      placeholder="Title"
-      name="title"
-      value={postForm.title}
-      onChangeText={(text) => handleInputChange("title", text)}
-      />
-      <TextInput style={style.input}
-      placeholder="Body"
-      name = "body"
-      value={postForm.body}
-      onChangeText={(text) => handleInputChange("body", text)}
-      />
-      <Button 
-      title={isPosting ? "Adding..." : "Add"} 
-      onPress={addPost}
-      disabled={isPosting}
-      />
-    </View>
-
-      <FlatList
-      ListEmptyComponent={() => <Text style={style.voidPosts}>No posts found</Text>}
-        data={posts}
-        renderItem={({ item }) => {
-          return (
-            <View style={style.card}>
-              <Text style={style.titleText}>
-                {item.title}
-              </Text>
-
-              <Text style={style.bodyText}>
-                {item.body}
-              </Text>
+      {
+        error ? 
+        <View style={style.errorContainer}>
+          <Text style={style.errorText}>{error}</Text>
+        </View> : (
+          <>
+            <View style={style.textInput}>
+              <TextInput style={style.input}
+                placeholder="Title"
+                name="title"
+                value={postForm.title}
+                onChangeText={(text) => handleInputChange("title", text)}
+              />
+              <TextInput style={style.input}
+                placeholder="Body"
+                name="body"
+                value={postForm.body}
+                onChangeText={(text) => handleInputChange("body", text)}
+              />
+              <Button
+                title={isPosting ? "Adding..." : "Add"}
+                onPress={addPost}
+                disabled={isPosting}
+              />
             </View>
-          )
-        }}
-        ItemSeparatorComponent={() => <View style={{height : 16}}></View>}
-        ListHeaderComponent={() => <Text style={style.header}>Posts</Text>}
-        ListFooterComponent={() => <Text style={style.footer}>End of Posts</Text>}
-        refreshing = {refreshing}
-        onRefresh={handleRefresh}
-      />
+
+            <FlatList
+              ListEmptyComponent={() => <Text style={style.voidPosts}>No posts found</Text>}
+              data={posts}
+              renderItem={({ item }) => {
+                return (
+                  <View style={style.card}>
+                    <Text style={style.titleText}>
+                      {item.title}
+                    </Text>
+
+                    <Text style={style.bodyText}>
+                      {item.body}
+                    </Text>
+                  </View>
+                )
+              }}
+              ItemSeparatorComponent={() => <View style={{ height: 16 }}></View>}
+              ListHeaderComponent={() => <Text style={style.header}>Posts</Text>}
+              ListFooterComponent={() => <Text style={style.footer}>End of Posts</Text>}
+              refreshing={refreshing}
+              onRefresh={handleRefresh}
+            />
+          </>
+        )
+      }
     </SafeAreaView>
   )
 }
 
 const style = StyleSheet.create({
-  container : {
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    paddingHorizontal: 12
+  },
+  card: {
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 8
+  },
+  titleText: {
+    fontWeight: "bold",
+    fontSize: 22,
+  },
+  bodyText: {
+    color: "#666666"
+  },
+  header: {
+    alignSelf: "center",
+    fontSize: 28,
+    textDecorationLine: "underline",
+    marginBottom: 10
+  },
+  footer: {
+    fontSize: 18,
+    alignSelf: "center",
+    marginVertical: 12,
+    fontWeight: "bold"
+  },
+  voidPosts: {
+    alignSelf: "center",
+    margin: "auto",
+    fontSize: 24
+  },
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textInput: {
+    padding: 10,
+    marginVertical: 12
+  },
+  input: {
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 8,
+    borderColor: "#66666"
+  },
+  errorContainer : {
     flex : 1,
-    paddingTop : StatusBar.currentHeight,
-    paddingHorizontal : 12
-  },
-  card : {
-    borderWidth : 1,
-    padding : 10,
-    borderRadius : 8
-  },
-  titleText : {
-    fontWeight : "bold",
-    fontSize : 22,
-  },
-  bodyText : {
-    color : "#666666"
-  },
-  header : {
-    alignSelf : "center",
-    fontSize : 28,
-    textDecorationLine : "underline",
-    marginBottom : 10
-  },
-  footer : {
-    fontSize : 18,
-    alignSelf : "center",
-    marginVertical : 12,
-    fontWeight : "bold"
-  },
-  voidPosts : {
-    alignSelf : "center",
-    margin : "auto",
-    fontSize : 24
-  },
-  loader : {
-    flex : 1,
+    alignItems : "center",
     justifyContent : "center",
-    alignItems : "center"
+    padding : 12
   },
-  textInput : {
-    padding : 10,
-    marginVertical : 12
-  },
-  input : {
-    borderWidth : 1,
-    padding : 10,
-    marginBottom : 10,
-    borderRadius : 8,
-    borderColor : "#66666"
+  errorText : {
+    fontSize : 24,
+    alignSelf : "center",
+    color : "red"
   }
 })
