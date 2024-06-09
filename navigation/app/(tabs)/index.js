a4eb2c4e63e44e28e7657b8989ca2293cbbf3c37
dev-rabin <robinmandhotia@gmail.com>
2024-06-09 20:12:25 +0530

    codeEvaluation playlist completed

--- navigation/app/(tabs)/index.js
+++ navigation/app/(tabs)/index.js
@@ -1,28 +1,36 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import DashboardScreen from "../../screens/DashboardScreen";
 const BottomNavigation = createBottomTabNavigator();
 import { Ionicons } from "@expo/vector-icons";
 import DashboardSettingsScreen from "../../screens/DashboardSettingsScreen";
 
-export default function App () {
-    return (
-        <BottomNavigation.Navigator screenOptions={
-            {
-                tabBarLabelPosition : "below-icon",
-                tabBarShowLabel : true,
-                tabBarActiveTintColor : "red",
-                tabBarInactiveTintColor : "blue",
-            }
-        }>
-        <BottomNavigation.Screen name="Bottom Tab Dashboard" component={DashboardScreen} 
+export default function App() {
+  return (
+    <BottomNavigation.Navigator
+      screenOptions={{
+        tabBarLabelPosition: "below-icon",
+        tabBarShowLabel: true,
+        tabBarActiveTintColor: "red",
+        tabBarInactiveTintColor: "blue",
+      }}
+    >
+      <BottomNavigation.Screen
+        name="Bottom Tab Dashboard"
+        component={DashboardScreen}
         options={{
-            tabBarIcon : ({color}) => <Ionicons name="person" size={20} color="purple"/>,
-            tabBarBadge:2
-        }}/>
-        <BottomNavigation.Screen name="Bottom Tab Setttings" component={DashboardSettingsScreen} options={{
-            tabBarIcon : () => <Ionicons name="settings" size={20}
-            />
-        }} />
-        </BottomNavigation.Navigator>
-    )
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" size={20} color="purple" />
+          ),
+          tabBarBadge: 2,
+        }}
+      />
+      <BottomNavigation.Screen
+        name="Bottom Tab Setttings"
+        component={DashboardSettingsScreen}
+        options={{
+          tabBarIcon: () => <Ionicons name="settings" size={20} />,
+        }}
+      />
+    </BottomNavigation.Navigator>
+  );
 }
--- navigation/screens/DashboardScreen.js
+++ navigation/screens/DashboardScreen.js
@@ -1,24 +1,22 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 
-export default function DashboardScreen ({naviagtion}) {
-    return (
-        <View style={styles.container}>
-        <Text style={styles.text}>
-        Dashboard Screen
-        </Text>
-        <Button title="Toggle Drawer"/>
-        </View>
-    )
+export default function DashboardScreen({ naviagtion }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Dashboard Screen</Text>
+      <Button title="Toggle Drawer" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flex : 1,
-        justifyContent : "center",
-        alignItems : "center"
-    },
-    text: {
-        fontSize : 30,
-        fontWeight : "bold"
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+});
